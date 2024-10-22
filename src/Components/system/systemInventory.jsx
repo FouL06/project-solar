be4c3a9617c82bf9b6bda03e54483821d1659dc8
Ashton Foulger <ashtonfoulger@gmail.com>
2024-10-22 14:42:06 -0600

    put panels into dropdown menu and updated cards container to match width

--- src/Components/system/systemInventory.jsx
+++ src/Components/system/systemInventory.jsx
@@ -1,58 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchSystemInventory } from "../../Redux/features/systemInventorySlice";
-import { Bolt } from "@mui/icons-material";
-import { Tooltip } from "@mui/material";
+import { Bolt, ExpandMore } from "@mui/icons-material";
+import {
+  Tooltip,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
 import "./systemInventory.css";
 
 const SystemInventory = () => {
   const dispatch = useDispatch();
-  const { envoys, inverters, meters } = useSelector(
-    (state) => state.systemInventory
-  );
+  const { inverters } = useSelector((state) => state.systemInventory);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     dispatch(fetchSystemInventory());
-    // const intervalId = setInterval(() => {
-    //   dispatch(fetchSystemInventory());
-    // }, 5 * 60 * 1000);
-    // return () => clearInterval(intervalId);
   }, [dispatch]);
 
-  // TODO: create hover card over panel to display info
-  // about the panel/inverter
+  const handleAccordionChange = () => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  };
 
   return (
     <div id="panel-container">
-      {inverters && inverters.length > 0 ? (
-        inverters.map((inverter, index) => (
-          <Tooltip
-            disableFocusListener
-            disableTouchListener
-            title={
-              "Serial #: " +
-              inverter.sn +
-              " - Model: " +
-              inverter.model +
-              " - SKU: " +
-              inverter.sku
-            }
-          >
-            <div key={index} className="panel-card">
-              <Bolt
-                style={{
-                  height: "45px",
-                  width: "45px",
-                  backgroundColor: "lightgreen",
-                  borderRadius: "50%",
-                }}
-              />
+      <Accordion
+        expanded={expanded}
+        onChange={handleAccordionChange}
+        className="centered-accordion"
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMore />}
+          aria-controls="panel-content"
+          id="panel-header"
+        >
+          <Typography>System Inventory</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {inverters && inverters.length > 0 ? (
+            <div className="panel-grid">
+              {inverters.map((inverter, index) => (
+                <Tooltip
+                  key={index}
+                  disableFocusListener
+                  disableTouchListener
+                  title={
+                    "Serial #: " +
+                    inverter.sn +
+                    " - Model: " +
+                    inverter.model +
+                    " - SKU: " +
+                    inverter.sku
+                  }
+                >
+                  <div className="panel-card">
+                    <Bolt
+                      style={{
+                        height: "45px",
+                        width: "45px",
+                        backgroundColor: "lightgreen",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </div>
+                </Tooltip>
+              ))}
             </div>
-          </Tooltip>
-        ))
-      ) : (
-        <p>No inverters found</p>
-      )}
+          ) : (
+            <p>No inverters found</p>
+          )}
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 };
